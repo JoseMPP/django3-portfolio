6517c7fdf439fa9929696f4d25d2552c08a57e8a
joseMPP <josmapizarro.99@gmail.com>
2022-03-23 11:28:23 -0400

    First Commit!

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    date = models.DateField(auto_now=True)
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
